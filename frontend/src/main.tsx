3996b17a9d6c7f5d5908d159b906e21b64c6a5cd
JafisGhussan88 <gjafis@gmail.com>
2025-04-12 06:33:07 +0700

    router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { Home } from './pages/home/Home.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { routes } from "./utils/root.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter(routes);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Home />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
