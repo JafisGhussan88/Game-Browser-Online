26c528a9e023e45b346baf4bd243bde7a96c51ad
Ghussan <gjafis@gmail.com>
2025-04-11 11:03:45 +0700

    laravel12

---
+++ database/migrations/2025_03_13_041219_create_tables_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tables', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('table_name');
+            $table->string('table_code');
+            $table->integer('qty');
+            $table->integer('price');
+            $table->boolean('is_reserved')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tables');
+    }
+};
---
+++ database/migrations/2025_03_13_041613_create_reservations_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('table_id')->constrained('tables')->onDelete('cascade');
+            $table->foreignUuid('user_id')->constrained('users')->onDelete('cascade');
+            $table->timestamp('start_date');
+            $table->timestamp('end_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
